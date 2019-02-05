93980a1bcb410ef0c1c5f916b4ada4309bb3fd21
karasb888 <Ivan.mazurenko99@gmail.com>
2019-02-05 12:36:04 +0200

    Fix urls

---
+++ precache-manifest.9071034dc212fca6975212f4a81ae9e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f4f630bf47f2ce8041",
+    "url": "/ReactDemo/static/css/main.1e6c4bbb.chunk.css"
+  },
+  {
+    "revision": "63f4f630bf47f2ce8041",
+    "url": "/ReactDemo/static/js/main.63f4f630.chunk.js"
+  },
+  {
+    "revision": "b3e7dc87b2a27212fafa",
+    "url": "/ReactDemo/static/js/1.b3e7dc87.chunk.js"
+  },
+  {
+    "revision": "a82ae06ee47d91df156c",
+    "url": "/ReactDemo/static/js/runtime~main.a82ae06e.js"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/ReactDemo/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/ReactDemo/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/ReactDemo/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "5dbcdacacbe7572d3f0b2105b555d0bf",
+    "url": "/ReactDemo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.91baaefbee5012d805901f11d3d32ed5.js"
+  "/ReactDemo/precache-manifest.9071034dc212fca6975212f4a81ae9e8.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ReactDemo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
