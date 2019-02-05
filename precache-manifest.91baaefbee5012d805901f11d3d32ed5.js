bb8aaa25263f1c01971e29fcfb7699d554f436a9
karasb888 <Ivan.mazurenko99@gmail.com>
2019-02-05 12:31:30 +0200

    Initial

---
+++ precache-manifest.91baaefbee5012d805901f11d3d32ed5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "63f4f630bf47f2ce8041",
+    "url": "/static/css/main.1e6c4bbb.chunk.css"
+  },
+  {
+    "revision": "63f4f630bf47f2ce8041",
+    "url": "/static/js/main.63f4f630.chunk.js"
+  },
+  {
+    "revision": "b3e7dc87b2a27212fafa",
+    "url": "/static/js/1.b3e7dc87.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "86c17a7097b302da04a1ab845ebec5ee",
+    "url": "/static/media/1st.86c17a70.svg"
+  },
+  {
+    "revision": "74629788d1ed8d1336b2899fe54d2db0",
+    "url": "/static/media/2nd.74629788.svg"
+  },
+  {
+    "revision": "98642d3cceaaf0e9bbe88916d5c2f82e",
+    "url": "/static/media/3rd.98642d3c.svg"
+  },
+  {
+    "revision": "336893fd7da12b67216fdf38678fc301",
+    "url": "/index.html"
+  }
+];
